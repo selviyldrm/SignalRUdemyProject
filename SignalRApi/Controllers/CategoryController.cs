00e46844c1a56e57cc25e84c67de385a3f302e2f
selviyldrm <selviyldrm96@gmail.com>
2023-12-01 20:18:55 +0300

    Add project files.

---
+++ SignalRApi/Controllers/CategoryController.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.CategoryDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
+
+        public CategoryController(ICategoryService categoryService, IMapper mapper)
+        {
+            _categoryService = categoryService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            var values = _mapper.Map<List<ResultCategoryDto>>(_categoryService.TGetListAll());
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            _categoryService.TAdd(new Category()
+            {
+                CategoryName = createCategoryDto.CategoryName,
+                Status = true,
+            });
+            return Ok("Kategori Eklendi");
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCategory(int id)
+        {
+            var values = _categoryService.TGetByID(id);
+            _categoryService.TDelete(values);
+            return Ok("Kategori Silindi");
+        }
+        [HttpPut]
+        public IActionResult UpdateCategory(UpdateCategoryDto updateCategoryDto)
+        {
+            _categoryService.TUpdate(new Category()
+            {
+                 CategoryID = updateCategoryDto.CategoryID,
+                CategoryName = updateCategoryDto.CategoryName,
+                Status =updateCategoryDto.Status
+            });
+            return Ok("Kategori Güncellendi");
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetCategory(int id)
+        {
+            var values = _categoryService.TGetByID(id);
+            return Ok(values);
+        }
+    }
+}
---
+++ SignalRApi/Controllers/ProductController.cs
@@ -0,0 +1,89 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+using SignalR.DtoLayer.ProductDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        private readonly IMapper _mapper;
+        public ProductController(IProductService productService, IMapper mapper)
+        {
+            _productService = productService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult ProductList()
+        {
+            var value = _mapper.Map<List<ResultProductDto>>(_productService.TGetListAll());
+            return Ok(value);
+        }
+        [HttpGet("ProductListWithCategory")]
+        public IActionResult ProductListWithCategory()
+        {
+            var context = new SignalRContext();
+            var value = context.Products.Include(x => x.Category).Select(y => new ResultProductWithCategoryDto
+            {
+                CategoryName=y.Category.CategoryName,
+                Description=y.Description,
+                Image=y.Image,
+                Price = y.Price,
+                ProductID=y.ProductID,
+                ProductName = y.ProductName,
+                ProductStatus = y.ProductStatus
+            });
+            return Ok(value.ToList());
+        }
+        [HttpPost]
+        public IActionResult CreateProduct(CreateProductDto createProductDto)
+        {
+            _productService.TAdd(new Product()
+            {
+                Description = createProductDto.Description,
+                Price = createProductDto.Price,
+                ProductName = createProductDto.ProductName,
+                ProductStatus = createProductDto.ProductStatus,
+                Image=createProductDto.Image,
+                CategoryID=createProductDto.CategoryID
+            });
+            return Ok("Ürün Bilgisi Eklendi");
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            var value = _productService.TGetByID(id);
+            _productService.TDelete(value);
+            return Ok("Ürün Bilgisi Silindi");
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            var value = _productService.TGetByID(id);
+            return Ok(value);
+        }
+        [HttpPut]
+        public IActionResult UpdateProduct(UpdateProductDto updateProductDto)
+        {
+            _productService.TUpdate(new Product()
+            {
+                Description = updateProductDto.Description,
+                Price = updateProductDto.Price,
+                ProductName = updateProductDto.ProductName,
+                ProductStatus = updateProductDto.ProductStatus,
+                ProductID = updateProductDto.ProductID,
+                Image=updateProductDto.Image,
+                CategoryID=updateProductDto.CategoryID
+            });
+            return Ok("Ürün Bilgisi Güncellendi");
+        }
+    }
+}
